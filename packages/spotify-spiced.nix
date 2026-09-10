eee3e6a36a254b4e4dee5de8ca8e1d2a3e25a234
aoesun <37470931+aoesun@users.noreply.github.com>
2026-09-10 22:22:55 +0900

    feat(spotify): enable Spicetify extensions and Marketplace
    - enable configured Spicetify extensions
    - add Marketplace as a custom app
    - provide a commented Catppuccin theme template

--- packages/spotify-spiced.nix
+++ packages/spotify-spiced.nix
@@ -3,4 +3,20 @@
   spicetify-nix,
 }:
 
-spicetify-nix.lib.mkSpicetify pkgs { }
+let
+  spicePkgs = spicetify-nix.legacyPackages.${pkgs.stdenv.hostPlatform.system};
+in
+spicetify-nix.lib.mkSpicetify pkgs {
+  enabledExtensions = with spicePkgs.extensions; [
+    adblockify
+    shuffle
+  ];
+
+  enabledCustomApps = with spicePkgs.apps; [
+    marketplace
+  ];
+
+  # Optional theme example. Uncomment both lines to enable it.
+  # theme = spicePkgs.themes.catppuccin;
+  # colorScheme = "mocha";
+}
